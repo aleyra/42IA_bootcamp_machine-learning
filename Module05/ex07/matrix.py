a7e9a39a3f6b47f56b50a3426d2c37dd190b20cd
lburnet <lburnet@ubuntu.myguest.virtualbox.org>
2023-07-13 13:43:48 +0200

    Module05 ex07 ok pas reussi a faire un muti constructor, pas grave

--- Module05/ex07/matrix.py
+++ Module05/ex07/matrix.py
@@ -1,38 +1,40 @@
 class Matrix:
     def __init__(self, obj) -> None:
-        # checks
-        print(obj)
-        if (isinstance(obj, list)):     
-            print("check 1 ok")
+        #init by values
+        # print(obj)
+        if (isinstance(obj, list)):
+            #checks
+            # print("bouh")
             nb_line = len(obj)
             if (nb_line == 0):
                 raise TypeError("Must be an list of lists of float or int")
             for i in range(nb_line):
                 if (not isinstance(obj[i], list)):
                     raise TypeError("Must be an list of lists of float or int")
-                print("check 2 ok")
                 nb_col = len(obj[0])
                 if (nb_col == 0):
                     raise TypeError("Must be an list of lists of float or int")
-                print("check 3 ok")
                 if (len(obj[i]) != nb_col):
                     raise TypeError(
                         "Each list in main list must have the same size")
-                print("check 4 ok")
                 for j in range(len(obj[i])):
                     if (not isinstance(obj[i][j], float)
                         and not isinstance(obj[i][j], int)):
                         raise TypeError(
                             "Must be an list of lists of float or int")
-                    print("check 5 ok")
+            #init
             self.shape = (nb_line, nb_col)
-        if (isinstance(obj, tuple)):
-            print("check 6 ok")
+            self.data = obj
+        #init by shape
+        elif (isinstance(obj, tuple)):
+            #checks
             if (len(obj) != 2):
                 raise TypeError("Shape must be a tuple of 2 int positive")
-            print("check 7 ok")
             if (not isinstance(obj[0], int) or not isinstance(obj[1], int)):
-                raise TypeError("Shape must be a tuple of 2 int positive")
+                raise TypeError("Shape must be a tuple of 2 int > 0")
+            if (obj[0] < 1 or obj[1] < 1):
+                raise TypeError("Shape must be a tuple of 2 int > 0")
+            #init
             self.shape = obj
             self.data = []
             for i in range(obj[0]):
@@ -46,9 +48,9 @@ class Matrix:
     # add : only matrices of same dimensions.
     def __add__(self, m):
         if (not isinstance(m, Matrix)):
-            raise TypeError("Add is between 2 matrix of same dimensions")
+            raise TypeError("Add is between 2 matrices of same dimensions")
         if (self.shape != m.shape):
-            raise TypeError("Add is between 2 matrix of same dimensions")
+            raise TypeError("Add is between 2 matrices of same dimensions")
         res = Matrix(m.shape)
         for i in range(m.shape[0]):
             for j in range(m.shape[1]):
@@ -109,20 +111,20 @@ class Matrix:
     # matrices,
     # returns a Vector if we perform Matrix * Vector mutliplication.
     def __mul__(self, l):
-        if (isinstance(l, int) or isinstance(l, float)):
+        if (isinstance(l, int) or isinstance(l, float)):#case scalar
             res = Matrix(self.shape)
             for i in range(self.shape[0]):
                 for j in range(self.shape[1]):
                     res.data[i][j] = self.data[i][j] * l
             return res
-        #case Vector TODO
-        elif (isinstance(l, Matrix)):
-            if (self.shape[0] != l.shape[1]):
+        #case Vector TODO or not because done in Vector's class
+        elif (isinstance(l, Matrix)):#case matrix
+            if (self.shape[1] != l.shape[0]):
                 raise TypeError(
                     "M x N is possible only if M's nb_line = N's nb_col")
-            res = Matrix((self.shape[0], self.shape[0]))
-            for i in range(self.shape[0]):
-                for j in range(self.shape[0]):
+            res = Matrix((self.shape[0], l.shape[1]))
+            for i in range(res.shape[0]):
+                for j in range(res.shape[1]):
                     for k in range(self.shape[1]):
                         res.data[i][j] += self.data[i][k] * l.data[k][j]
             return res
@@ -139,14 +141,13 @@ class Matrix:
                 for j in range(self.shape[1]):
                     res.data[i][j] = r * self.data[i][j]
             return res
-        #case Vector TODO
-        elif (isinstance(r, Matrix)):
-            if (r.shape[0] != self.shape[1]):
+        elif (isinstance(r, Matrix)):#case matrix
+            if (r.shape[1] != self.shape[0]):
                 raise TypeError(
                     "M x N is possible only if M's nb_line = N's nb_col")
-            res = Matrix((r.shape[0], r.shape[0]))
-            for i in range(r.shape[0]):
-                for j in range(r.shape[0]):
+            res = Matrix((r.shape[0], self.shape[1]))
+            for i in range(res.shape[0]):
+                for j in range(res.shape[1]):
                     for k in range(r.shape[1]):
                         res.data[i][j] += r.data[i][k] * self.data[k][j]
             return res
@@ -169,3 +170,225 @@ class Matrix:
             to_print += f"\n\t{self.data[i]}"
         to_print += "\n]"
         return to_print
+        # return repr(self.data)
+    
+    def T(self):
+        res = Matrix((self.shape[1],self.shape[0]))
+        for i in range(res.shape[0]):
+            for j in range(res.shape[1]):
+                res.data[i][j] = self.data[j][i]
+        return res
+    
+class Vector(Matrix):
+    def __init__(self, obj) -> None:
+        #init by shape
+        if (isinstance(obj, tuple)):
+            #checks
+            if (len(obj) != 2):
+                raise TypeError("Shape must be a tuple of 2 int positive")
+            if (not isinstance(obj[0], int) or not isinstance(obj[1], int)):
+                raise TypeError("Shape must be a tuple of 2 int positive")
+            if ((obj[0] != 1 and obj[1] != 1) or obj[0] < 1 or obj[1] < 1):
+                raise TypeError("Shape must be (1,n) or (n,1)")
+            #init
+            self.shape = obj
+            self.data = []
+            for i in range(self.shape[0]):
+                t = []
+                for j in range(self.shape[1]):
+                    t.append(0)
+                self.data.append(t)
+        #init by values
+        elif (isinstance(obj, list)):
+            if (len(obj) != 1):#case column
+                #checks
+                for i in range(len(obj)):
+                    if (not isinstance(obj[i], list)):
+                        raise TypeError("the list must contain lists of 1 "
+                            + "float or 1 list of several floats")
+                    if (len(obj[i]) != 1):
+                        raise TypeError(
+                            "the list must contain lists of 1 float")
+                    if (not isinstance(obj[i][0], int)
+                        and not isinstance(obj[i][0], float)):
+                        raise TypeError(
+                            "the list must contain lists of 1 float")
+                #init
+                self.data = obj
+                self.shape = (len(obj), 1)
+            else:#case line
+                #checks
+                if (not isinstance(obj[0], list)):
+                    raise TypeError("the list must contain 1 list of floats")
+                for i in range(len(obj[0])):
+                    if (not isinstance(obj[0][i], int)
+                        and not isinstance(obj[0][i], float)):
+                        raise TypeError(
+                            "the list must contain 1 list of floats")
+                #init
+                self.data = obj
+                self.shape = (1, len(obj[0]))
+        else:
+            raise TypeError("Must init with a list or a tuple")
+    
+    def __add__(self, v):
+        if (not isinstance(v, Vector)):
+            raise TypeError("Add is between 2 vectors of same dimensions")
+        if (self.shape != v.shape):
+            raise TypeError("Add is between 2 vectors of same dimensions")
+        res = Vector(v.shape)
+        for i in range(v.shape[0]):
+            for j in range(v.shape[1]):
+                res.data[i][j] = self.data[i][j] + v.data[i][j]
+        return res
+    
+    def __radd__(self, v):
+        if (not isinstance(v, Vector)):
+            raise TypeError("Add is between 2 vectors of same dimensions")
+        if (self.shape != v.shape):
+            raise TypeError("Add is between 2 vectors of same dimensions")
+        res = Vector(v.shape)
+        for i in range(v.shape[0]):
+            for j in range(v.shape[1]):
+                res.data[i][j] = v.data[i][j] + self.data[i][j]
+        return res
+    
+    def __sub__(self, v):
+        if (not isinstance(v, Vector)):
+            raise TypeError("Sub is between 2 vectors of same dimensions")
+        if (self.shape != v.shape):
+            raise TypeError("Sub is between 2 vectors of same dimensions")
+        res = Vector(v.shape)
+        for i in range(v.shape[0]):
+            for j in range(v.shape[1]):
+                res.data[i][j] = self.data[i][j] - v.data[i][j]
+        return res
+    
+    def __rsub__(self, v):
+        if (not isinstance(v, Vector)):
+            raise TypeError("Sub is between 2 vectors of same dimensions")
+        if (self.shape != v.shape):
+            raise TypeError("Sub is between 2 vectors of same dimensions")
+        res = Vector(v.shape)
+        for i in range(v.shape[0]):
+            for j in range(v.shape[1]):
+                res.data[i][j] = v.data[i][j] - self.data[i][j]
+        return res
+    
+    def __truediv__(self, l):
+        if (not isinstance(l, int) and not isinstance(l, float)):
+            raise TypeError(
+                "TrueDiv is only between a vector and a scalar (int or float)")
+        if (l == 0):
+            raise ZeroDivisionError("Division by zero impossible")
+        res = Vector(self.shape)
+        for i in range(self.shape[0]):
+            for j in range(self.shape[1]):
+                res.data[i][j] = self.data[i][j] / l
+        return res
+    
+    def __rtruediv__(self, l):
+        raise NotImplementedError("Impossible to divise a scalar by a vector")
+    
+    def __mul__(self, l):
+        if (isinstance(l, int) or isinstance(l, float)):#case scalar
+            res = Vector(self.shape)
+            for i in range(self.shape[0]):
+                for j in range(self.shape[1]):
+                    res.data[i][j] = self.data[i][j] * l
+            return res
+        elif (isinstance(l, Vector)):#case Vector
+            if (self.shape[0] != 1):
+                raise TypeError(
+                    "Impossible to do v x u when v is a vector column")
+            if (l.shape[1] != 1):
+                raise TypeError(
+                    "Impossible to do v x u when u is a vector line")
+            if (self.shape[1] != l.shape[0]):
+                raise TypeError(
+                    "Impossible to do v x u when v's nb_col != u's nb_line")
+            res = Vector((1,1))
+            for i in range(l.shape[0]):
+                res.data[0][0] += self.data[0][i] * l.data[i][0]
+            return res
+        elif (isinstance(l, Matrix)):#case matrix
+            if (self.shape[0] != 1):
+                raise TypeError(
+                    "Impossible to do v x M when v is a vector column")
+            if (self.shape[1] != l.shape[0]):
+                raise TypeError(
+                    "v x M is possible only if M's nb_line = v's nb_col")
+            res = Vector((self.shape[0], l.shape[1]))
+            for i in range(res.shape[0]):
+                for j in range(res.shape[1]):
+                    for k in range(self.shape[1]):
+                        res.data[i][j] += self.data[i][k] * l.data[k][j]
+            return res
+        else:
+            raise TypeError("""Multiplication is possible between :
+        - vector and scalar
+        - vector and vector
+        - vector and matrix""")
+
+    def __rmul__(self, r):
+        if (isinstance(r, int) or isinstance(r, float)):#case scalar
+            res = Vector(self.shape)
+            for i in range(self.shape[0]):
+                for j in range(self.shape[1]):
+                    res.data[i][j] = r * self.data[i][j]
+            return res
+        elif (isinstance(r, Vector)):#case Vector
+            if (self.shape[0] != 1):
+                raise TypeError(
+                    "Impossible to do v x u when v is a vector column")
+            if (r.shape[1] != 1):
+                raise TypeError(
+                    "Impossible to do v x u when u is a vector line")
+            if (r.shape[1] != self.shape[0]):
+                raise TypeError(
+                    "Impossible to do v x u when v's nb_col != u's nb_line")
+            res = Vector((1,1))
+            for i in range(self.shape[0]):
+                res.data[0][0] += r.data[0][i] * self.data[i][0]
+            return res
+        elif (isinstance(r, Matrix)):#case matrix
+            if (self.shape[1] != 1):
+                raise TypeError(
+                    "Impossible to do M x v when v is a vector line")
+            if (r.shape[1] != self.shape[0]):
+                raise TypeError(
+                    "v x M is possible only if M's nb_line = v's nb_col")
+            res = Vector((r.shape[0], self.shape[1]))
+            for i in range(res.shape[0]):
+                for j in range(res.shape[1]):
+                    for k in range(r.shape[1]):
+                        res.data[i][j] += r.data[i][k] * self.data[k][j]
+            return res
+        else:
+            raise TypeError("""Multiplication is possible between :
+        - scalar and vector
+        - vector and vector
+        - matrix and vector""")
+
+    def dot(self, v):
+        if (not isinstance(v, Vector)):
+            raise TypeError(
+                "dot product is possible only between 2 vectors of same shape")
+        if (v.shape != self.shape):
+            raise TypeError(
+                "dot product is possible only between 2 vectors of same shape")
+        res = 0.
+        if (v.shape[0] == 1):
+            for i in range(v.shape[1]):
+                res += self.data[0][i] * v.data[0][i]
+        else:
+            for i in range(v.shape[0]):
+                res += self.data[i][0] * v.data[i][0]
+        return res
+
+    def T(self):
+        res = Vector((self.shape[1], self.shape[0]))
+        for i in range(res.shape[0]):
+            for j in range(res.shape[1]):
+                res.data[i][j] = self.data[j][i]
+        return res
--- Module05/ex07/test.py
+++ Module05/ex07/test.py
@@ -1,10 +1,108 @@
 from matrix import Matrix
+from matrix import Vector
 
 if __name__ == "__main__":
-    l1 = [[1.0, 2.0], [3.0, 4.0]]
-    l2 = [[1, 'a'],[3, 3], [4, 4]]
-    # print(l1)
-    # m1 = Matrix(l1)
-    # print(m1)
+    #Matrices
+    #init matrix + __str__ + __repr__
+    l1 = [[1.0, 2.0], [3.0, 4.0], [5, 6]]
+    m1 = Matrix(l1)
+    print(m1)
     m2 = Matrix((3, 3))
-    print(m2)
+    # print(repr(m2))
+
+    #add
+    l3 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    m3 = Matrix(l3)
+    m3x2 = m3 + m3
+    # print(m3x2 + m3)
+    # print(m1 + m2)# error
+    # print(m2 + m1)# error
+    # print(m1 + 2)# error
+    # print(2 + m1)# error
+
+    #sub
+    # print(m3 - m3x2)
+    # print(m1 - m3)# error
+    # print(m3 - m1)# error
+    # print(m1 - 2)# error
+    # print(2 - m1)# error
+
+    #truediv
+    # print(m1 / 2)
+    # print(m1 / m1)# error
+    # print(2 / m1)# error
+
+    #mul
+    l4 = [[1, 1], [1, 1]]
+    m4 = Matrix(l4)
+    # print(m1 * 0)
+    # print(m1 * m1)# error
+    # print(m1 * m3)# error
+    # print(m3 * m1)
+    # print(m4 * m4)
+
+    #T
+    # print(m1)
+    m5 = m1.T()
+    # print(m5)
+
+    #Vectors
+    #init
+    lv1 = [[1, 2, 3]]
+    v1 = Vector(lv1)
+    print(v1)
+    lv2 = [[1], [2], [3]]
+    v2 = Vector(lv2)
+    print(repr(v2))
+    tv3 = (1, 2)
+    v3 = Vector(tv3)
+    # print(v3)
+
+    #add
+    v1x2 = v1 + v1
+    # print(v1x2)
+    # print(type(v1x2))
+    # print(v1 + v2)
+    # print(v1 + 2)
+    # print(v1 + m1)
+
+    #sub
+    # print(v1x2 - v1)
+    # print(v1 - v1x2)
+    # print(type(v1 - v1x2))
+    # print(v1 - 1)# error
+    # print(1 - v1)# error
+    # print(v1 - m1)# error
+    # print(m1 - v1)# error
+
+    #truediv
+    # print(v1 / 2)
+    # print(type(v1 / 2))
+    # print(2 / v1)# error
+    # print(v1 / v1)# error
+    # print(v1 / m1)# error
+    # print(m1 / v1)# error
+
+    #mul
+    # print(v1 * 2)
+    # print(type(v1 * 2))
+    # print(2 * v1)
+    # print(v1 * v1)# error
+    # print(v1 * v2)
+    # print(v1 * m1)
+    # print(m1 * v1)# error
+    # print(v2 * m1)# error
+    # print(m1 * v2)# error
+    # print(v2 * m1)# error
+    # print(m5 * v2)
+    # print(v2 * v1)# error
+    # print(v2 * v2)# error
+
+    #dot
+    # print(v1.dot(v1))
+    # print(v1.dot(v2))# error
+    # print(v1.dot(2))# error
+
+    #T
+    # print(v1.T())
+    # print(type(v1.T()))
