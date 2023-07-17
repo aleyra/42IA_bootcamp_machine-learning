b3fa1f2890462ef5d2adb007e6c4280460166fe8
lburnet <lburnet@student.42lyon.fr>
2023-07-17 17:17:17 +0200

    Module02 ex04 j'ai appliqué les formules comme demandé mais ça donne des resultats différents... curieux

--- Module06/ex04/z_score.py
+++ Module06/ex04/z_score.py
@@ -1,30 +1,42 @@
 import numpy as np
-
+import math
+from TinyStatistician import TinyStatistician
 
 def zscore(x):
-    """Computes the normalized version of a non-empty numpy.ndarray using the z-score standardization.
+    """Computes the normalized version of a non-empty numpy.ndarray using the
+    z-score standardization.
     Args:
             x: has to be an numpy.ndarray, a vector.
     Returns:
-            x' as a numpy.ndarray.
-            None if x is a non-empty numpy.ndarray or not a numpy.ndarray.
+            x_prime as a numpy.ndarray.
+            None if x is not a non-empty numpy.ndarray or not a numpy.ndarray.
     Raises:
             This function shouldn't raise any Exception.
     """
-    print("bouh")
-
+    if not isinstance(x, np.ndarray):
+        return None
+    if x.size == 0:
+        return None
+    
+    tstat = TinyStatistician()
+    mean = tstat.mean(x)
+    std = tstat.std(x)
+    x_prime = np.ndarray(x.shape)
+    for i in range(x.shape[0]):
+        x_prime[i] = (x[i] - mean) / std
+    return x_prime
 
 if __name__ == "__main__":
     # Example 1:
     X = np.array([0, 15, -9, 7, 12, 3, -21])
-    zscore(X)
+    print(zscore(X))
     # Output:
     # array([-0.08620324, 1.2068453 , -0.86203236, 0.51721942, 0.94823559,
     # 0.17240647, -1.89647119])
-    
+
     # Example 2:
     Y = np.array([2, 14, -13, 5, 12, 4, -19]).reshape((-1, 1))
-    zscore(Y)
+    print(zscore(Y))
     # Output:
     # array([ 0.11267619, 1.16432067, -1.20187941, 0.37558731, 0.98904659,
     # 0.28795027, -1.72770165])
