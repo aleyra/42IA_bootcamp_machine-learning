4c3f5d19185d1821216ad5d5de2d7c1512be2bcd
lburnet <lburnet@ubuntu.myguest.virtualbox.org>
2023-07-18 16:20:56 +0200

    Module07 ex02 ok ex2 verif

--- Module07/ex02/gradient.py
+++ Module07/ex02/gradient.py
@@ -1,6 +1,36 @@
 import numpy as np
 
 
+def add_intercept(x):
+    """Adds a column of 1's to the non-empty numpy.array x.
+    Args:
+        to be a numpy.array of dimension m * n.
+    Returns:
+        X, a numpy.array of dimension m * (n + 1).
+        None if x is not a numpy.array.
+        None if x is an empty numpy.array.
+    Raises:
+        function should not raise any Exception.
+    """
+    if not isinstance(x, np.ndarray) or x.size == 0:
+        return None
+    if x.ndim == 1:
+        res = []
+        nb_line = x.shape[0]
+        for i in range(nb_line):
+            res.append(1)
+            res.append(x[i])
+        res = np.array(res)
+        res = res.reshape(nb_line, 2)
+        return res
+
+    col1 = list()
+    for i in range(x.shape[0]):
+        col1.append(1)
+    res = np.insert(x, 0, col1, axis=1)
+    return res
+
+
 def gradient(x, y, theta):
     """Computes a gradient vector from three non-empty numpy.array, without
     any for-loop.
@@ -18,7 +48,22 @@ def gradient(x, y, theta):
     Raises:
             This function should not raise any Exception.
     """
-    return None
+    if (
+        not isinstance(x, np.ndarray)
+        or not isinstance(y, np.ndarray)
+        or not isinstance(theta, np.ndarray)
+    ):
+        return None
+    if x.size == 0 or y.size == 0 or theta.size == 0:
+        return None
+    x_prime = add_intercept(x)
+    print(f"x' = {x_prime}")
+    if x.shape[0] != y.shape[0] or x.shape[1] + 1 != theta.shape[0]:  # ?
+        return None
+    x_prime_T = np.transpose(x_prime)
+    prodmat = np.matmul(x_prime, theta)
+    gradient = np.matmul(x_prime_T, prodmat - y) / x.shape[0]
+    return gradient
 
 
 if __name__ == "__main__":
@@ -34,14 +79,16 @@ if __name__ == "__main__":
     y = np.array([2, 14, -13, 5, 12, 4, -19]).reshape((-1, 1))
     theta1 = np.array([3,0.5,-6]).reshape((-1, 1))
 
+    print(theta1)
+
     # Example 1:
-    g = gradient(x, y, theta1)
-    print(f"exemple 1\n{g}")
+    # g = gradient(x, y, theta1)
+    # print(f"exemple 1\n{g}")
     # Output:
     # array([[ -33.71428571], [ -37.35714286], [183.14285714], [-393.]])
 
     # Example 2:
-    theta2 = np.array([0,0,0]).reshape((-1, 1))
+    theta2 = np.array([0,0,0, 0]).reshape((-1, 1))
     g = gradient(x, y, theta2)
     print(f"exemple 2\n{g}")
     # Output:
