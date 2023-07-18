f2000d59b092669f178caead6fc896a2125b51a8
lburnet <lburnet@ubuntu.myguest.virtualbox.org>
2023-07-18 11:37:39 +0200

    Module07 prep

---
+++ Module07/ex06/polynomial_model.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+
+def add_polynomial_features(x, power):
+    """Add polynomial features to vector x by raising its values up to the
+    power given in argument.
+    Args:
+            x: has to be an numpy.array, a vector of dimension m * 1.
+            power: has to be an int, the power up to which the components of
+            vector x are going to be raised.
+    Return:
+            The matrix of polynomial features as a numpy.array,
+            of dimension m * n,
+            containing the polynomial feature values for all training examples.
+            None if x is an empty numpy.array.
+            None if x or power is not of expected type.
+    Raises:
+            This function should not raise any Exception.
+    """
+    return None
+
+
+if __name__ == "__main__":
+    x = np.arange(1,6).reshape(-1, 1)
+
+    # Example 0:
+    apf = add_polynomial_features(x, 3)
+    print(f"exemple 0\n{apf}")
+    # Output:
+    # array([[ 1, 1, 1],
+    #     [ 2, 4, 8],
+    #     [ 3, 9, 27],
+    #     [ 4, 16, 64],
+    #     [ 5, 25, 125]])
+
+    # Example 1:
+    apf = add_polynomial_features(x, 6)
+    print(f"exemple 1\n{apf}")
+    # Output:
+    # array([[ 1, 1, 1, 1, 1, 1],
+    #     [ 2, 4, 8, 16, 32, 64],
+    #     [ 3, 9, 27, 81, 243, 729],
+    #     [ 4, 16, 64, 256, 1024, 4096],
+    #     [ 5, 25, 125, 625, 3125, 15625]])
---
+++ Module07/ex07/polynomial_train.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from polynomial_model import add_polynomial_features
+from mylinearregression import MyLinearRegression as MyLR
+
+
+if __name__ == "__main__":
+    # begin???
+    x = np.arange(1,11).reshape(-1,1)
+    y = np.array([[ 1.39270298],
+        [ 3.88237651],
+        [ 4.37726357],
+        [ 4.63389049],
+        [ 7.79814439],
+        [ 6.41717461],
+        [ 8.63429886],
+        [ 8.19939795],
+        [10.37567392],
+        [10.68238222]])
+    
+    plt.scatter(x,y)
+    plt.show()
+
+    # Build the model:
+    x_ = add_polynomial_features(x, 3)
+    my_lr = MyLR(np.ones(4).reshape(-1,1)).fit_(x_, y)
+
+    # Plot:
+    ## To get a smooth curve, we need a lot of data points
+    continuous_x = np.arange(1,10.01, 0.01).reshape(-1,1)
+    x_ = add_polynomial_features(continuous_x, 3)
+    y_hat = my_lr.predict_(continuous_x)
+    
+    plt.scatter(x,y)
+    plt.plot(continuous_x, y_hat, color='orange')
+    plt.show()
+    # end???
+
+    # starting points ?
+    theta4 = np.array([[-20],[ 160],[ -80],[ 10],[ -1]]).reshape(-1,1)
+    theta5 = np.array(
+        [[1140],[ -1850],[ 1110],[ -305],[ 40],[ -2]]).reshape(-1,1)
+    theta6 = np.array(
+        [[9110],[ -18015],[ 13400],[ -4935],[ 966],[ -96.4],[ 3.86]]
+    ).reshape(-1,1)
