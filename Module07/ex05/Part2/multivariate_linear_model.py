880276011c695477e4e70cc1bec6968dfd9b6f07
lburnet <lburnet@ubuntu.myguest.virtualbox.org>
2023-07-20 10:20:48 +0200

    Module07 ex05 part2 ouin

--- Module07/ex05/Part2/multivariate_linear_model.py
+++ Module07/ex05/Part2/multivariate_linear_model.py
@@ -4,18 +4,22 @@ from mylinearregression import MyLinearRegression as MyLR
 
 
 if __name__ == "__main__":
-    data = pd.read_csv("../data/spacecraft_data.csv")
+    data = pd.read_csv("../../data/spacecraft_data.csv")
     X = np.array(data[['Age', 'Thrust_power', 'Terameters']])
     Y = np.array(data[['Sell_price']])
+    # print(f"X = {X}")
     my_lreg = MyLR(
-        theta = [1.0, 1.0, 1.0, 1.0], , alpha = 1e-4, max_iter = 600000
+        theta = [1.0, 1.0, 1.0, 1.0], alpha = 1e-4, max_iter = 600000
     )
 
     # Example 0:
-    m = my_lreg.mse_(X,Y)
+    y_hat = my_lreg.predict_(X)
+    m = my_lreg.mse_(Y, y_hat)
     print(f"exemple 0\n{m}")
     # Output:
     # 144044.877...
+
+    # my_lreg.gradient(X,Y)
 
     # Example 1:
     my_lreg.fit_(X,Y)
@@ -24,7 +28,7 @@ if __name__ == "__main__":
     # array([[334.994...],[-22.535...],[5.857...],[-2.586...]])
 
     # Example 2:
-    m = my_lreg.mse_(X,Y)
-    print(f"exemple 2\n{m}")
+    # m = my_lreg.mse_(X,Y)
+    # print(f"exemple 2\n{m}")
     # Output:
     # 586.896999...
---
+++ Module07/ex05/Part2/mylinearregression.py
@@ -0,0 +1,211 @@
+import numpy as np
+
+
+class MyLinearRegression():
+    """
+    Description:
+            An improves version of MyLinearRegression class made in
+            Module06/ex02
+    """
+    def __init__(self, theta, alpha=0.001, max_iter=1000):
+        self.alpha = alpha
+        self.max_iter = max_iter
+        # print(type(theta))
+        if (isinstance(theta, list)):
+            self.theta = np.array(theta)
+            if (self.theta.shape != (len(self.theta), 1)):
+                self.theta = self.theta.reshape((len(self.theta), 1))
+        else:
+            self.theta = theta
+        # print(f"theta = {self.theta.shape}")
+
+    def add_intercept(self, x):
+        """Adds a column of 1's to the non-empty numpy.array x.
+        Args:
+            to be a numpy.array of dimension m * n.
+        Returns:
+            X, a numpy.array of dimension m * (n + 1).
+            None if x is not a numpy.array.
+            None if x is an empty numpy.array.
+        Raises:
+            function should not raise any Exception.
+        """
+        if not isinstance(x, np.ndarray) or x.size == 0:
+            return None
+        if x.ndim == 1:
+            res = []
+            nb_line = x.shape[0]
+            for i in range(nb_line):
+                res.append(1)
+                res.append(x[i])
+            res = np.array(res)
+            res = res.reshape(nb_line, 2)
+            return res
+
+        col1 = list()
+        for i in range(x.shape[0]):
+            col1.append(1)
+        res = np.insert(x, 0, col1, axis=1)
+        return res
+    
+    def gradient(self, x, y):
+        """Computes a gradient vector from three non-empty numpy.array, without
+        any for-loop.
+        The three arrays must have the compatible dimensions.
+        Args:
+                x: has to be an numpy.array, a matrix of dimension m * n.
+                y: has to be an numpy.array, a vector of dimension m * 1.
+                theta: has to be an numpy.array, a vector (n +1) * 1.
+        Return:
+                The gradient as a numpy.array, a vector of dimensions n * 1,
+                containg the result of the formula for all j.
+                None if x, y, or theta are empty numpy.array.
+                None if x, y and theta do not have compatible dimensions.
+                None if x, y or theta is not of expected type.
+        Raises:
+                This function should not raise any Exception.
+        """
+        if (
+            not isinstance(x, np.ndarray)
+            or not isinstance(y, np.ndarray)
+            or not isinstance(self.theta, np.ndarray)
+        ):
+            return None
+        if x.size == 0 or y.size == 0 or self.theta.size == 0:
+            return None
+        if x.shape[0] != y.shape[0] or x.shape[1] + 1 != self.theta.shape[0]:
+            return None
+        x_prime = self.add_intercept(x)
+        x_prime_T = np.transpose(x_prime)
+        prodmat = np.matmul(x_prime, self.theta)
+        x_prime_T = np.array(x_prime_T, dtype = np.float64)
+        prodmat = np.array(prodmat, dtype = np.float64)
+        # print(f"x_prime_T = {x_prime_T}")
+        gradient = np.matmul(x_prime_T, prodmat - y) / x.shape[0]
+        return gradient
+    
+    def fit_(self, x, y):
+        """
+        Description:
+                Fits the model to the training dataset contained in x and y.
+        Args:
+                x: has to be a numpy.array, a matrix of dimension m * n:
+                (number of training examples, number of features).
+                y: has to be a numpy.array, a vector of dimension m * 1:
+                (number of training examples, 1).
+                theta: has to be a numpy.array, a vector
+                of dimension (n + 1) * 1: (number of features + 1, 1).
+                alpha: has to be a float, the learning rate
+                max_iter: has to be an int, the number of iterations done
+                during the gradient descent
+        Return:
+                new_theta: numpy.array, a vector of dimension
+                (number of features + 1, 1).
+                None if there is a matching dimension problem.
+                None if x, y, theta, alpha or max_iter is not of expected type.
+        Raises:
+                This function should not raise any Exception.
+        """
+        if (
+            not isinstance(x, np.ndarray)
+            or not isinstance(y, np.ndarray)
+            or not isinstance(self.theta, np.ndarray)
+            or not isinstance(self.alpha, float)
+            or not isinstance(self.max_iter, int)
+        ):
+            return None
+        if x.ndim != 2 or y.ndim != 2 or self.theta.ndim != 2:
+            return None
+        if self.alpha > 1 or self.alpha < 0 or self.max_iter < 1:
+            return None
+        if x.size == 0 or y.size == 0 or self.theta.size == 0:
+            return None
+        if x.shape[0] != y.shape[0] or x.shape[1] + 1 != self.theta.shape[0]:
+            return None
+        print("bouh")
+        
+        for i in range(self.max_iter):
+            self.theta = self.theta - self.alpha * self.gradient(x, y)
+        return self.theta
+
+    def predict_(self, x):
+        """Computes the prediction vector y_hat from two non-empty numpy.array.
+        Args:
+                x: has to be an numpy.array, a vector of dimensions m * n.
+                theta: has to be an numpy.array, a vector of
+                dimensions (n + 1) * 1
+        Return:
+                y_hat as a numpy.array, a vector of dimensions m * 1.
+                None if x or theta are empty numpy.array.
+                None if x or theta dimensions are not appropriate.
+                None if x or theta is not of expected type.
+        Raises:
+                This function should not raise any Exception.
+        """
+        if (
+            not isinstance(x, np.ndarray)
+            or not isinstance(self.theta, np.ndarray)
+        ):
+            return None
+        if x.size == 0 or self.theta.size == 0:
+            return None
+        if self.theta.shape[1] != 1 or x.shape[1] + 1 != self.theta.shape[0]:
+            return None
+        y_hat = np.ndarray((x.shape[0], 1))
+        x = self.add_intercept(x)
+        y_hat = np.matmul(x, self.theta)
+        return y_hat
+    
+    def loss_(self, y, y_hat):
+        """Computes the mean squared error of two non-empty numpy.array, without
+        any for loop.
+        The two arrays must have the same dimensions.
+        Args:
+                y: has to be an numpy.array, a vector.
+                y_hat: has to be an numpy.array, a vector.
+        Return:
+                The mean squared error of the two vectors as a float.
+                None if y or y_hat are empty numpy.array.
+                None if y and y_hat does not share the same dimensions.
+                None if y or y_hat is not of expected type.
+        Raises:
+                This function should not raise any Exception.
+        """
+        if not isinstance(y, np.ndarray) or not isinstance(y_hat, np.ndarray):
+            return None
+        if y.size == 0 or y_hat.size == 0:
+            return None
+        if y.shape != y_hat.shape:
+            return None
+        diff = y_hat - y
+        res = 0.0
+        for i in range(diff.shape[0]):
+            res += diff[i][0] * diff[i][0]
+        res /= 2 * diff.shape[0]
+        return res
+    
+    def mse_(self, y, y_hat):
+        """
+        Description:
+                Calculate the MSE between the predicted output and the real
+                output.
+        Args:
+                y: has to be a numpy.array, a vector of dimension m * 1.
+                y_hat: has to be a numpy.array, a vector of dimension m * 1.
+        Returns:
+                mse: has to be a float.
+                None if there is a matching dimension problem.
+        Raises:
+                This function should not raise any Exceptions.
+        """
+        if not isinstance(y, np.ndarray) or not isinstance(y_hat, np.ndarray):
+            return None
+        if y.size == 0 or y_hat.size == 0:
+            return None
+        if y.shape != y_hat.shape:
+            return None
+        res = 0.0
+        for i in range(y.shape[0]):
+            res += (y_hat[i][0] - y[i][0]) * (y_hat[i][0] - y[i][0])
+        res /= y.shape[0]
+        return res
