b47bb640ae84888f12b07718c355b5f72aa6f0d1
lburnet <lburnet@ubuntu.myguest.virtualbox.org>
2023-07-28 11:51:43 +0200

    Module07 ex09 split data done

--- Module07/ex09/benchmark_train.py
+++ Module07/ex09/benchmark_train.py
@@ -6,4 +6,16 @@ from data_spliter import data_spliter
 from search_alpha_max_iter import search_alpha_max_iter as sami
 
 if __name__ == "__main__":
+    # from csv to np.ndarray
+    data = pd.read_csv("../data/space_avocado.csv")
+    Ytarget = np.array(data['target']).reshape(-1,1)
+    X = np.array(data[['weight', 'prod_distance', 'time_delivery']])
+
+    # split in training set and test set
+    split = data_spliter(X, Ytarget, 0.8)
+    X_training = split[0]
+    Y_training = split[2]
+    X_test = split[1]
+    Y_test = split[3]
+
     
