6eaaef75e99ab4203904d6590659e18592b4f847
lburnet <lburnet@ubuntu.myguest.virtualbox.org>
2023-08-04 10:24:12 +0200

    Module09 ex00 ok

--- Module09/ex00/polynomial_model_extended.py
+++ Module09/ex00/polynomial_model_extended.py
@@ -15,13 +15,29 @@ def add_polynomial_features(x, power):
     Raises:
             This function should not raise any Exception.
     """
+    if not isinstance(x, np.ndarray) or not isinstance(power, int):
+        return None
+    if x.size == 0 or x.ndim != 2 or power <= 0:
+        return None
+    ret = x
+    for i in range(2, power + 1):
+        x_power_i = np.power(x[:,0], i).reshape(-1, 1)
+        for j in range(1, x.shape[1]):
+            xj_power_i = np.power(x[:,j], i).reshape(-1, 1)
+            x_power_i = np.append(x_power_i, xj_power_i, axis = 1)
+        ret = np.concatenate((ret, x_power_i), axis=1)
+    return ret
+
 
 if __name__ == "__main__":
     x = np.arange(1,11).reshape(5, 2)
+    print(x)
 
     # Example 1:
-    add_polynomial_features(x, 3)
+    bouh = add_polynomial_features(x, 3)
+    print(bouh)
     
     # Example 2:
-    add_polynomial_features(x, 4)
+    bouh = add_polynomial_features(x, 4)
+    print(bouh)
     
