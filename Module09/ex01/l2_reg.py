e36750f02e5b36a39538da791f8eb41d7c97bd82
lburnet <lburnet@ubuntu.myguest.virtualbox.org>
2023-08-04 11:21:22 +0200

    Module09 ex01 ok

--- Module09/ex01/l2_reg.py
+++ Module09/ex01/l2_reg.py
@@ -12,6 +12,18 @@ def iterative_l2(theta):
     Raises:
             This function should not raise any Exception.
     """
+    if (
+        not isinstance(theta, np.ndarray)
+        or theta.size == 0
+        or theta.shape[1] != 1
+    ):
+        return None
+    l2 = 0
+    theta_prime = theta
+    theta_prime[0][0] = 0
+    for i in range(theta_prime.shape[0]):
+        l2 += theta_prime[i][0] * theta_prime[i][0]
+    return l2
 
 
 def l2(theta):
@@ -25,29 +37,38 @@ def l2(theta):
     Raises:
             This function should not raise any Exception.
     """
+    if (
+        not isinstance(theta, np.ndarray)
+        or theta.size == 0
+        or theta.shape[1] != 1
+    ):
+        return None
+    theta_prime = theta
+    theta_prime[0][0] = 0
+    l2 = np.dot(np.transpose(theta_prime), theta_prime)
+    return l2[0][0]
 
 
 if __name__ == "__main__":
     x = np.array([2, 14, -13, 5, 12, 4, -19]).reshape((-1, 1))
-    
+
     # Example 1:
-    iterative_l2(x)
+    print(iterative_l2(x))
     # Output:
     911.0
-    
+
     # Example 2:
-    l2(x)
+    print(l2(x))
     # Output:
     911.0
-    y = np.array([3,0.5,-6]).reshape((-1, 1))
-    
+    y = np.array([3, 0.5, -6]).reshape((-1, 1))
+
     # Example 3:
-    iterative_l2(y)
-    # Output:
-    36.25
-    
-    # Example 4:
-    l2(y)
+    print(iterative_l2(y))
     # Output:
     36.25
 
+    # Example 4:
+    print(l2(y))
+    # Output:
+    36.25
