0d72318cdf5445c40d3969ab7d32d9eb398369a0
lburnet <lburnet@ubuntu.myguest.virtualbox.org>
2023-08-04 13:58:09 +0200

    Module09 ex03 ok

--- Module09/ex03/logistic_loss_reg.py
+++ Module09/ex03/logistic_loss_reg.py
@@ -1,4 +1,6 @@
 import numpy as np
+from l2_reg import l2
+from vec_log_loss import vec_log_loss_
 
 
 def reg_log_loss_(y, y_hat, theta, lambda_):
@@ -27,25 +29,27 @@ def reg_log_loss_(y, y_hat, theta, lambda_):
         return None
     if y.shape != y_hat.shape or y.shape[1] != 1 or theta.shape[1] != 1:
         return None
-
+    m = y.shape[0]
+    loss = vec_log_loss_(y, y_hat) + lambda_ / (2 * m) * l2(theta)
+    return loss
 
 
 if __name__ == "__main__":
     y = np.array([1, 1, 0, 0, 1, 1, 0]).reshape((-1, 1))
     y_hat = np.array([.9, .79, .12, .04, .89, .93, .01]).reshape((-1, 1))
     theta = np.array([1, 2.5, 1.5, -0.9]).reshape((-1, 1))
-    
+
     # Example :
-    reg_log_loss_(y, y_hat, theta, .5)
+    print(reg_log_loss_(y, y_hat, theta, .5))
     # Output:
     0.43377043716475955
-    
+
     # Example :
-    reg_log_loss_(y, y_hat, theta, .05)
+    print(reg_log_loss_(y, y_hat, theta, .05))
     # Output:
     0.13452043716475953
-    
+
     # Example :
-    reg_log_loss_(y, y_hat, theta, .9)
+    print(reg_log_loss_(y, y_hat, theta, .9))
     # Output:
     0.6997704371647596
